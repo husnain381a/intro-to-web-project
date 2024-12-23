063ae23601761c1c5d58caacab43c0e0b1ab6e53
Mazhar <Mazhar@DESKTOP-S88HF4T>
2024-12-23 22:50:53 +0500

    Add project files.

---
+++ OPEN SKILLS SOURCE (MVC PROJECT)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OPEN_SKILLS_SOURCE__MVC_PROJECT_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
